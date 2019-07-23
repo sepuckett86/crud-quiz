40cd866c60133581c1065f642eac840cffbbb732
sepuckett86 <sepuckett86@gmail.com>
2019-07-23 11:15:00 -0700

    add JamSession model, post test, and route

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crud Quiz
+
+Goal: create a CRUD back-end with Node, Express, Mongoose, and MongoDB
+
+Theme: Jam Sessions for musicians
---
+++ test/jam-sessions.test.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('posts a jam session', () => {
+    return request(app)
+      .post('/api/v1/jam-sessions')
+      .send({
+        where: 'Jesuit High School',
+        when: new Date('July 30, 2019'),
+        who: ['Allision', 'Brady', 'Chris']
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          where: 'Jesuit High School',
+          when: new Date('July 30, 2019').toISOString(),
+          who: ['Allision', 'Brady', 'Chris'],
+          __v: 0
+        });
+      });
+  });
+});
